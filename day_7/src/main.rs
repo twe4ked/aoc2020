849dd90ade214ac98c23c70b0b8557e5d5500fac
Odin Dutton <odindutton@gmail.com>
2020-12-08 21:29:11 +1100

    Day 7 (part 2)

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -42,6 +42,39 @@
 //
 // How many bag colors can eventually contain at least one shiny gold bag? (The list of rules is
 // quite long; make sure you get all of it.)
+//
+// --- Part Two ---
+//
+// It's getting pretty expensive to fly these days - not because of ticket prices, but because of
+// the ridiculous number of bags you need to buy!
+//
+// Consider again your shiny gold bag and the rules from the above example:
+//
+//     faded blue bags contain 0 other bags.
+//     dotted black bags contain 0 other bags.
+//     vibrant plum bags contain 11 other bags: 5 faded blue bags and 6 dotted black bags.
+//     dark olive bags contain 7 other bags: 3 faded blue bags and 4 dotted black bags.
+//
+// So, a single shiny gold bag must contain 1 dark olive bag (and the 7 bags within it) plus 2
+// vibrant plum bags (and the 11 bags within each of those): 1 + 1*7 + 2 + 2*11 = 32 bags!
+//
+// Of course, the actual rules have a small chance of going several levels deeper than this
+// example; be sure to count all of the bags, even if the nesting becomes topologically
+// impractical!
+//
+// Here's another example:
+//
+// shiny gold bags contain 2 dark red bags.
+// dark red bags contain 2 dark orange bags.
+// dark orange bags contain 2 dark yellow bags.
+// dark yellow bags contain 2 dark green bags.
+// dark green bags contain 2 dark blue bags.
+// dark blue bags contain 2 dark violet bags.
+// dark violet bags contain no other bags.
+//
+// In this example, a single shiny gold bag must contain 126 other bags.
+//
+// How many individual bags are required inside your single shiny gold bag?
 
 fn main() {
     let input = include_str!("../input")
@@ -52,13 +85,17 @@ fn main() {
     let part_1 = part_1(&input);
     assert_eq!(part_1, 139);
     println!("Part 1: {}", part_1);
+
+    let part_2 = part_2(&input);
+    assert_eq!(part_2, 58175);
+    println!("Part 2: {}", part_2);
 }
 
 use petgraph::graphmap::DiGraphMap;
 use std::collections::HashSet;
 
-fn graph<'a>(input: &'a Vec<String>) -> DiGraphMap<&'a str, ()> {
-    let mut graph: DiGraphMap<&str, ()> = Default::default();
+fn graph<'a>(input: &'a Vec<String>) -> DiGraphMap<&'a str, usize> {
+    let mut graph: DiGraphMap<&str, usize> = Default::default();
 
     for line in input {
         let mut iter = line.split(" bags contain ");
@@ -76,18 +113,18 @@ fn graph<'a>(input: &'a Vec<String>) -> DiGraphMap<&'a str, ()> {
             rest.split(", ")
                 .map(|rule| {
                     let mut iter = rule.splitn(2, ' ');
-                    let _count = iter.next().unwrap();
+                    let count: usize = iter.next().unwrap().parse().unwrap();
 
                     let rest = iter.next().unwrap();
                     let mut iter = rest.split(" bag"); // bags.
 
-                    iter.next().unwrap()
+                    (iter.next().unwrap(), count)
                 })
                 .collect()
         };
 
-        for rule in rules {
-            graph.add_edge(name, rule, ());
+        for (rule, count) in rules {
+            graph.add_edge(name, rule, count);
         }
     }
 
@@ -102,7 +139,7 @@ fn graph<'a>(input: &'a Vec<String>) -> DiGraphMap<&'a str, ()> {
 fn part_1(input: &Vec<String>) -> usize {
     let graph = graph(input);
 
-    fn neighbors<'a>(graph: &DiGraphMap<&'a str, ()>, node: &'a str) -> HashSet<&'a str> {
+    fn neighbors<'a>(graph: &DiGraphMap<&'a str, usize>, node: &'a str) -> HashSet<&'a str> {
         graph
             .neighbors_directed(node, petgraph::Direction::Incoming)
             .map(|n| {
@@ -115,6 +152,25 @@ fn part_1(input: &Vec<String>) -> usize {
     }
 
     neighbors(&graph, &"shiny gold").len()
+}
+
+fn part_2(input: &Vec<String>) -> usize {
+    let graph = graph(input);
+
+    fn sum_bag_counts<'a>(graph: &DiGraphMap<&'a str, usize>, node: &'a str) -> usize {
+        let incoming = graph.neighbors_directed(node, petgraph::Direction::Outgoing);
+
+        incoming
+            .map(|n| {
+                let count = *graph.edge_weight(node, n).unwrap();
+                let inner_bags_count = sum_bag_counts(graph, n);
+
+                count + (count * inner_bags_count)
+            })
+            .sum::<usize>()
+    }
+
+    sum_bag_counts(&graph, &"shiny gold")
 }
 
 #[cfg(test)]
@@ -139,5 +195,6 @@ mod tests {
         .collect();
 
         assert_eq!(part_1(&input), 4);
+        assert_eq!(part_2(&input), 32);
     }
 }
